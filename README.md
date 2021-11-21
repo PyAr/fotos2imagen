1b745938e618da66a22c04f53e2c96e6c69e06da
Carlos Matías de la Torre <carlos.delatorre@mercadolibre.com>
2021-11-20 22:54:29 -0300

    WIP on the first prototype

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fotos2imagen
 La idea es armar una imagen pero compuesta por miles de fotitos. Jugar y aprender.
+
---
+++ script.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import random
+
+import numpy as np
+
+from PIL import Image
+
+
+class Fisa:
+    def prepare(self, images_for_comparison):
+        return self
+
+    def select_candidate_image(self, x):
+        
+        return random.choice(["x.jpg","facu200.jpg"])
+
+
+
+if __name__ == "__main__":
+    source_image = Image.open(sys.argv[1])
+    chunk_size = int(sys.argv[2])
+    tile_size = int(sys.argv[3])
+    out_fname = sys.argv[4]
+
+    images_for_comparison = f"data/{chunk_size}x{chunk_size}/"  # Se deriva de chunk_size
+    target_images_directory = f"data/{tile_size}x{tile_size}/"  # Se deriva de tile_size
+
+    fisa = Fisa().prepare(images_for_comparison)
+
+    # Corto la imagen original para que entren chunks justos
+    width, height = source_image.size
+    width_in_chunks = width // chunk_size
+    height_in_chunks = height // chunk_size
+    n_chunks = width_in_chunks * height_in_chunks
+    exceeding_cols = width % chunk_size
+    exceeding_rows = height % chunk_size
+    print(f"Entran {n_chunks} chunks")
+    print(f"Entran {width_in_chunks} chunks a lo largo y sobran {str(exceeding_cols)} columnas")
+    print(f"Entran {height_in_chunks} chunks a lo alto y sobran {str(exceeding_rows)} filas")
+
+    box = (0, 0, width - exceeding_cols, height - exceeding_rows)
+    useful_region = np.array(source_image.crop(box))
+    # redefine
+    width, height, _ = useful_region.shape
+
+    
+    mosaic_width = tile_size * width_in_chunks
+    mosaic_height = tile_size * height_in_chunks
+    mosaic = Image.new("RGB", (mosaic_width, mosaic_height), (255, 255, 255))
+
+    for chunk_col in range(width_in_chunks):
+        for chunk_row in range(height_in_chunks):
+            # Chunk ini coords
+            col_ini = chunk_col * chunk_size
+            row_ini = chunk_row * chunk_size
+            print(f"Chunk {chunk_col}, {chunk_row}: ({col_ini}, {row_ini})")
+
+            chunk = useful_region[col_ini: col_ini + chunk_size, row_ini: row_ini + chunk_size]
+            print(chunk.shape)
+            filename = fisa.select_candidate_image(chunk)  # Fisa's
+            target_image_fname = os.path.join(target_images_directory, filename)
+            target_image = Image.open(target_image_fname)
+            
+            start_x = chunk_col * tile_size
+            start_y = chunk_row * tile_size
+            mosaic.paste(target_image, (start_x, start_y))
+
+    mosaic.save(out_fname)
