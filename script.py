a70794deaf0184e20174e78fb12c3190ad9f9f53
Carlos Matías de la Torre <carlos.delatorre@mercadolibre.com>
2021-11-21 11:47:25 -0300

    Working version

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import sys
 import pickle
 
@@ -11,9 +12,13 @@ from pathlib import Path
 from finder import ImageFinder
 
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 
 ROOT_DATA_DIR = "./downloads/processed/"
-DEFAULT_FISA_FNAME = "fisa.pkl"
+DEFAULT_FISA_TEMPLATE = "fisa-{chunk_size}.pkl"
 
 
 def extract_chuk(chunk_col, chunk_row, chunk_size):
@@ -30,8 +35,6 @@ class Mosaiker:
         self.images_for_comparison = os.path.join(ROOT_DATA_DIR, f"{chunk_size}x{chunk_size}/")  # Se deriva de chunk_size
         self.target_images_directory = os.path.join(ROOT_DATA_DIR, f"{tile_size}x{tile_size}/")  # Se deriva de tile_size
 
-        self.fisa = self._load_a_fisa()
-
         # Corto la imagen original para que entren chunks justos
         width, height = source_image.size
         width_in_chunks = width // chunk_size
@@ -44,9 +47,9 @@ class Mosaiker:
         print(f"Entran {height_in_chunks} chunks a lo alto y sobran {str(exceeding_rows)} filas")
 
         box = (0, 0, width - exceeding_cols, height - exceeding_rows)
-        useful_region = np.array(source_image.crop(box))
+        useful_region = source_image.crop(box)
         # redefine
-        width, height, _ = useful_region.shape
+        width, height = useful_region.size
 
         self.useful_region = useful_region
         self.width = width
@@ -55,66 +58,76 @@ class Mosaiker:
         self.width_in_chunks = width_in_chunks
         self.height_in_chunks = height_in_chunks
         self.n_chunks = n_chunks
+        self.tile_size = tile_size
         
+        self.fisa = self._load_a_fisa()
+
         mosaic_width = tile_size * width_in_chunks
         mosaic_height = tile_size * height_in_chunks
         self._mosaic = Image.new("RGB", (mosaic_width, mosaic_height), (255, 255, 255))
 
     def _load_a_fisa(self):
-        if os.path.exists(DEFAULT_FISA_FNAME):
-            print("Found a Fisa! Let's use it...")
-            with open(DEFAULT_FISA_FNAME, "rb") as f:
+        target_fisa = DEFAULT_FISA_TEMPLATE.format(chunk_size=self.chunk_size)
+        if os.path.exists(target_fisa):
+            print(f"Found a Fisa for chunks size {self.chunk_size}! Let's use it...")
+            with open(target_fisa, "rb") as f:
                 return pickle.load(f)
 
         fisa = ImageFinder(
             images_path=Path(self.images_for_comparison), 
-            window_height=chunk_size, 
-            window_width=chunk_size,
+            window_height=self.chunk_size, 
+            window_width=self.chunk_size,
         )
         # only once
         fisa.prepare()
-        with open(DEFAULT_FISA_FNAME, "wb") as f:
+        with open(target_fisa, "wb") as f:
             pickle.dump(fisa, f)
         return fisa
 
     def do_it(self):
         """just do it"""
 
-        self.tiles = []
-        for chunk_col in tqdm(range(self.width_in_chunks)):
-            for chunk_row in range(self.height_in_chunks):
-                chunk = self._extract_chunk(chunk_col, chunk_row)
-                filename = self.fisa.find_best_fit(chunk)  # Fisa's
-                self.tiles.append((chunk_col, chunk_row, filename))
-
-        self._put_tiles()
+        #import ipdb; ipdb.set_trace()
+        try:
+            for chunk_row in tqdm(list(range(self.height_in_chunks))):
+                #print(f"Chunk Fila {chunk_row}")
+                for chunk_col in range(self.width_in_chunks):
+                    #print(f"Chunk Columna {chunk_col}")
+                    chunk = self._extract_chunk(chunk_col, chunk_row)
+                    filename = self.fisa.find_best_fit(chunk)  # Fisa's
+                    self._put_single_tile_in_mosaic(chunk_col, chunk_row, filename)
+        except ValueError as e:
+            logger.exception(e)
+            print("Falló. A llorar al campito 😭")
+        
         
     def _extract_chunk(self, chunk_col, chunk_row):
         """Extract a chunk from the original image."""
 
         col_ini = chunk_col * self.chunk_size
         row_ini = chunk_row * self.chunk_size
-        #print(f"Chunk {chunk_col}, {chunk_row}: ({col_ini}, {row_ini})")
-        chunk = self.useful_region[col_ini: col_ini + self.chunk_size, row_ini: row_ini + self.chunk_size]
-        return chunk
-    
-    def _put_tiles(self):
-        """Put a single tile in the output mosaic."""
-        for chunk_col, chunk_row, filename in self.tiles:
-            target_image_fname = os.path.join(self.target_images_directory, filename)
-            target_image = Image.open(target_image_fname)
-            start_x = chunk_col * tile_size
-            start_y = chunk_row * tile_size
-            self._mosaic.paste(target_image, (start_y, start_x))
-            
+        # En numpy las coords son (y, x) o sea (row, col)
+        box = (col_ini, row_ini, col_ini + self.chunk_size, row_ini + self.chunk_size)
+        #print("Chunk box: " + str(box))
+        chunk = self.useful_region.crop(box)
+        return np.array(chunk)
+
+    def _put_single_tile_in_mosaic(self, chunk_col, chunk_row, filename):
+        target_image_fname = os.path.join(self.target_images_directory, filename)
+        target_image = Image.open(target_image_fname)
+        start_col = chunk_col * self.tile_size
+        start_row = chunk_row * self.tile_size
+        tile_upper_left_coords = (start_col, start_row)
+        #print("Tile box: " + str(tile_upper_left_coords))
+        self._mosaic.paste(target_image, tile_upper_left_coords)
         self._mosaic.save(out_fname)
 
 
 if __name__ == "__main__":
     source_image = Image.open(sys.argv[1])
-    chunk_size = int(sys.argv[2])
-    tile_size = int(sys.argv[3])
+    chunk_size_arg = int(sys.argv[2])
+    tile_size_arg = int(sys.argv[3])
     out_fname = sys.argv[4]
 
-    mosaiker = Mosaiker(source_image, chunk_size, tile_size, out_fname)
+    mosaiker = Mosaiker(source_image, chunk_size_arg, tile_size_arg, out_fname)
     mosaiker.do_it()
