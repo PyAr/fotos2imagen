4b4913709fd40e0a9d8e684bb31511a7114c1c11
Juan Pedro Fisanotti <fisadev@gmail.com>
2021-11-20 21:31:41 -0300

    A similar image finder

---
+++ finder.py
@@ -0,0 +1,60 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from tqdm import tqdm
+from PIL import Image
+
+
+class ImageFinder:
+    """
+    A finder able to find the most similar image (from a folder), to a given
+    window.
+    """
+    def __init__(self, images_path, window_height, window_width):
+        self.images_path = images_path
+        self.window_height = window_height
+        self.window_width = window_width
+
+        self.dataset = None
+        self.index_to_path = None
+        self.classifier = None
+
+    def prepare(self):
+        """
+        Prepare the images finder, precaching the source images and the
+        classifier used to match them.
+        """
+        self._build_sources_dataset()
+        self._build_classifier()
+
+    def _build_sources_dataset(self):
+        """
+        Given a pathlib path of a folder with images, create a dataset of
+        sources that can be used to find best fits for a window.
+        """
+        dataset = []
+        self.index_to_path = {}
+        for image_index, image_path in tqdm(list(enumerate(self.images_path.iterdir()))):
+            image = Image.open(image_path)
+            image_data = np.array(image.resize((self.window_width, self.window_height))).flatten()
+            dataset.append(image_data)
+            self.index_to_path[image_index] = image_path
+
+        self.dataset = np.array(dataset)
+
+    def _build_classifier(self):
+        """
+        Given a dataset of images, build a classifier able to find best fits
+        from them.
+        """
+        self.classifier = KNeighborsClassifier(n_neighbors=1)
+        self.classifier.fit(self.dataset, range(len(self.dataset)))
+
+    def find_best_fit(self, window):
+        """
+        Find the most similar image from the dataset, compared to a window.
+        Return the path to the found image.
+        """
+        inputs = np.array([window.flatten(), ])
+        predictions = self.classifier.predict(inputs)
+        best_fit_index = predictions[0]
+        return self.index_to_path[best_fit_index].name
