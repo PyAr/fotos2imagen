18f262334371309c2edb9ab346a41d616074b395
Matias <elmatibarriento@gmail.com>
2021-11-22 11:36:50 -0300

    Refactor of progress window

--- image_progress.py
+++ image_progress.py
@@ -2,17 +2,19 @@ import threading
 import time
 import tkinter as tk
 from pathlib import Path
-from tkinter import ttk
-from tqdm import tqdm
 from random import choice
-from PIL import Image, ImageTk, ImageOps
+from tkinter import ttk
 
-class App(threading.Thread):
+from PIL import Image, ImageOps, ImageTk
+from tqdm import tqdm
 
-    def __init__(self, base_image, width, height, *args, **kwargs):
+
+class ThreadingWindow(threading.Thread):
+    """Wrapper class for TK that allows using it in a thread an call actions outsidenthe mainoop"""
+
+    def __init__(self, width, height, *args, **kwargs):
         threading.Thread.__init__(self)
         self.start()
-        self._base_image = base_image
         self._height = 800
         self._ratio = width / height
         self._width = int(self._height * self._ratio)
@@ -36,32 +38,38 @@ class App(threading.Thread):
 
         self.root.mainloop()
 
-    def update_image(self):
-        image = self._fit_image(Image.open(self._base_image))
+    def update_image(self, base_image):
+        """Update the window canvas with a new image"""
+        image = self._fit_image(Image.open(base_image))
         self.canvas.itemconfig(self.image_on_canvas, image=image)
     
     def _fit_image(self, image):
+        """Fit the image to the window size"""
         fitted_image = ImageOps.fit(image, (self._width, self._height))
         self.photo_image = ImageTk.PhotoImage(fitted_image)
         return self.photo_image
 
-class image_progress_tdqm(tqdm):
+
+class image_progress(tqdm):
+    """tqdm implementation that update the image in the window
+    
+    It is in lower snake case to mimic tqdm progress
+    """
 
     def __init__(self, base_image, width, height, show_progress_window=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._base_image = base_image
+        
         self._with_window = show_progress_window
+        
         if self._with_window:
             self._base_image = str(Path(base_image).absolute())
-            self.app = App(base_image=base_image, width=width, height=height)
+            self.app = ThreadingWindow(width=width, height=height)
 
     def update(self,n=1):
         super().update(n=n)
         
         if self._with_window:
-            self.app.update_image()
-
-image_progress = image_progress_tdqm
+            self.app.update_image(self._base_image)
 
 __all__ = ["image_progress"]
-
-
